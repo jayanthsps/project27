e4649a60847f3057cfef80d5a64ab16faa28e873
jayanthsps <76771253+jayanthsps@users.noreply.github.com>
2021-03-16 14:02:17 +0530

    Add files via upload

---
+++ Rope.js
@@ -0,0 +1,29 @@
+class Rope{
+    constructor(body1,body2,offsetX,offsetY){
+        this.offsetX=offsetX
+        this.offsetY=offsetY
+        var options={
+            bodyA:body1,
+            bodyB:body2,
+            pointB:{x:this.offsetX , y:this.offsetY}
+        }
+
+        this.Rope=Matter.Constraint.create(options);
+        World.add(world,this.Rope);
+    }
+     display(){
+        var point1=this.Rope.bodyA.position
+        var point2=this.Rope.bodyB.position
+
+        strokeWeight(2);
+
+        var Anchor1X=point1.x;
+        var Anchor1Y=point1.y;
+
+        var Anchor2X=point2.x+this.offsetX;
+        var Anchor2Y=point2.y+this.offsetY;
+
+        line(Anchor1X,Anchor1Y,Anchor2X,Anchor2Y)
+
+    }
+}
